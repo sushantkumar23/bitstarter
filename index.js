bbc5fb0cba448fe5703699649662f12660c1ad79
Sushant Kumar <sushantkumar23@gmail.com>
2015-07-16 15:43:57 +0000

    changed index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(print)
+  response.send($print)
 })
 
 app.listen(app.get('port'), function() {
