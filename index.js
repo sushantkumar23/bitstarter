578ca109d3c5f356330e79909fdd80d1340a9839
Sushant Kumar <sushantkumar23@gmail.com>
2015-07-16 15:41:30 +0000

    changed index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var app = express();
+var buf = new Buffer ();
+buf = fs.readFileSync(index.html);
+var print = new String()
+print = buf.toString();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(print)
 })
 
 app.listen(app.get('port'), function() {
